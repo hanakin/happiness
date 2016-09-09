1fde115e2a76f5b9bc891fe2f63e829588f9870e
Michael Miday <hanakin@users.noreply.github.com>
2016-09-09 13:23:22 +0200

    Update options.js

--- options.js
+++ options.js
@@ -4,13 +4,13 @@ var pkg = require('./package.json');
 
 module.exports = {
 	bugs: pkg.bugs.url,
-	cmd: 'happiness',
+	cmd: 'sane',
 	eslint: eslint,
 	eslintConfig: {
 		configFile: path.join(__dirname, 'eslintrc.json')
 	},
-	formatter: 'Formatting is no longer included with standard. Install it separately: "npm install -g happiness-format"',
+	formatter: 'Formatting is no longer included with standard. Install it separately: "npm install -g sane-format"',
 	homepage: pkg.homepage,
-	tagline: 'Use JavaScript Happiness Style',
+	tagline: 'Use JavaScript Sane Style',
 	version: pkg.version
 };
