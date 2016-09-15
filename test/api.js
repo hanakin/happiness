d4ddcfa3162b5aeba86ce3532529adbcac836c16
Michael Miday <midaym@gmail.com>
2016-09-15 05:15:58 +0200

    revert changes to tests

--- test/api.js
+++ test/api.js
@@ -1,7 +1,5 @@
-var path = require('path');
 var sane = require('../');
 var test = require('tape');
-var filePath = path.resolve('./bin/cmd.js');
 
 test('api: lintFiles', function (t) {
     t.plan(3);
@@ -18,15 +16,5 @@ test('api: lintText', function (t) {
         t.error(err, 'no error while linting');
         t.equal(typeof result, 'object', 'result is an object');
         t.equal(result.errorCount, 1, 'should have used single quotes');
-    });
-});
-
-test('api: usage', function (t) {
-    t.plan(6);
-    sane.lintFiles([], { cwd: 'bin' }, function (err, result) {
-        t.error(err, 'no error while linting');
-        t.equal(typeof result, 'object', 'result is an object');
-        t.equal(result.errorCount, 0, 'error count 0');
-        t.equal(path.resolve(result.results[0].filePath), filePath, 'error filepath correct');
     });
 });
