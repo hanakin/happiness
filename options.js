336419b3cfbd4fa16a043f0b706642d6e93bedaf
Michael Miday <midaym@gmail.com>
2016-09-15 04:37:02 +0200

    update tests and cmds

--- options.js
+++ options.js
@@ -4,7 +4,7 @@ var pkg = require('./package.json');
 
 module.exports = {
     bugs: pkg.bugs.url,
-    cmd: 'sane-standard',
+    cmd: 'sane',
     eslint: eslint,
     eslintConfig: {
         configFile: path.join(__dirname, 'eslintrc.json')
--- test/api.js
+++ test/api.js
@@ -1,9 +1,11 @@
-var standard = require('../');
+var path = require('path');
+var sane = require('../');
 var test = require('tape');
+var filePath = path.resolve('./bin/cmd.js');
 
 test('api: lintFiles', function (t) {
     t.plan(3);
-    standard.lintFiles([], { cwd: 'bin' }, function (err, result) {
+    sane.lintFiles([], { cwd: 'bin' }, function (err, result) {
         t.error(err, 'no error while linting');
         t.equal(typeof result, 'object', 'result is an object');
         t.equal(result.errorCount, 0);
@@ -12,9 +14,19 @@ test('api: lintFiles', function (t) {
 
 test('api: lintText', function (t) {
     t.plan(3);
-    standard.lintText('console.log("hi there");\n', function (err, result) {
+    sane.lintText('console.log("hi there");\n', function (err, result) {
         t.error(err, 'no error while linting');
         t.equal(typeof result, 'object', 'result is an object');
         t.equal(result.errorCount, 1, 'should have used single quotes');
+    });
+});
+
+test('api: usage', function (t) {
+    t.plan(6);
+    sane.lintFiles([], { cwd: 'bin' }, function (err, result) {
+        t.error(err, 'no error while linting');
+        t.equal(typeof result, 'object', 'result is an object');
+        t.equal(result.errorCount, 0, 'error count 0');
+        t.equal(path.resolve(result.results[0].filePath), filePath, 'error filepath correct');
     });
 });
